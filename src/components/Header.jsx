ed5f1e212a39e9fc5213217a43ea7d352ee7e554
Mykhailo <mishajlovovrinv@gmail.com>
2024-09-27 17:15:29 +0300

    add animation to the buttons in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ export default function Header(){
             <div className="wrapper">
                 <div className="headerLogoWrapper"><img src={DeltaImg} alt="logo Delta" className="headerLogo"/></div>
                 <nav>
-                    <ul className="flex gap-5">
+                    <ul>
                         <li><a href="">Регіон</a></li>
                         <li><a href="">Тарифи</a></li>
                         <li><a href="">Інформація</a></li>
                         <li><a href="">Контакти</a></li>
                     </ul>
-                    <ul className="flex gap-5">
+                    <ul>
                         <li><a href="">Увійти</a></li>
                         <li><a href="">UA</a></li>
                     </ul>
