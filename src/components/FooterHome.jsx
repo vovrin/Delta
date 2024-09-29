e2d135b0542e121b60d96dabae786369116ddf0f
Mykhailo <mishajlovovrinv@gmail.com>
2024-09-29 13:24:12 +0300

    add footer

--- src/components/FooterHome.jsx
+++ src/components/FooterHome.jsx
@@ -1,7 +1,39 @@
+import Delta_white from "../assets/imgs/Delta_white.svg"
 export default function FooterHome(){
     return(
         <>
-            <h1>footer</h1>
+            <footer>
+                <div className="footerHomeCont wrapper">
+                    <div className="footerInf1">
+                        <img src={Delta_white} alt="" />
+                        <ul>
+                            <li>Тех підтримка</li>
+                            <li><a href="tel:+380691234567">Тел.: +380691234567</a></li>
+                            <li><a href="mailto:customersupport@delta.com.ua">Електронна пошта:<br></br>
+                            customersupport@delta.com.ua</a></li>
+                            <li><a href="mailto:job@delta.com.ua">Для співробітництва:<br></br>
+                            job@delta.com.ua</a></li>
+                            <li><a href="mailto:collaboration@delta.com.ua">collaboration@delta.com.ua</a></li>
+                        </ul>
+                    </div>
+                    <div className="footerInf2">
+                        <ul>
+                            <li><a href="">Регіон</a></li>
+                            <li><a href="">Тарифи</a></li>
+                            <li><a href="">Автомобілі</a></li>
+                            <li><a href="">Інформація</a></li>
+                            <li><a href="">Котакти</a></li>
+                        </ul>
+                    </div>
+                    <div className="footerInf3">
+                        <ul>
+                            <li><a href="">Увійти</a></li>
+                            <li><a href="">Мова</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import Section from "../../components/Section";
 import AboutCard from "../../components/AboutCard";
 import FooterHome from "../../components/FooterHome";
 import SwiperHome from "../../components/SwiperHome";
-
 export default function Home(){
     return(
         <>
@@ -16,6 +15,74 @@ export default function Home(){
             <AboutCard></AboutCard>
             
             <SwiperHome></SwiperHome>
+
+            <div className="useGuideHome">
+                <div className="wrapper">
+                    <p>Як користуватися Delta</p>
+                    <ul>
+                        <li>Створити аканут в Delta</li>
+                        <li>Обрати на мапінайближче до тебе авто.</li>
+                        <li>Обирай авто, розблоковуй його за допомогою додатку, рушай</li>
+                        <li>Завершуй поїздку в призначеному для цього місці, вказаному на мапі</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="tarrifs wrapper">
+                <div className="logoTarrifAb"><p>Тарифи</p></div>
+                <div className="frTarrif">
+                    <div className="purpHeader">
+                        <span>
+                            <p>Похвилинная</p>
+                            <h1>23123213123123</h1>
+                        </span>
+                        
+                        <p>Найкраще підходить для коротких поїздок</p>
+                    </div>
+                    <div className="addonsCard">
+                        <ul>
+                            <li>Включений пробіг</li>
+                            <li>Бронювання</li>
+                            <li>Очікування</li>
+                            <li>Понад пробігу</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="scTarrif">
+                    <div className="pinkHeader">
+                        <span>
+                            <p>Похвилинная</p>
+                            <h1>23123213123123</h1>
+                        </span>
+                        <p>Найкраще підходить для коротких поїздок</p>
+                    </div>
+                    <div className="addonsCard">
+                        <ul>
+                            <li>Включений пробіг</li>
+                            <li>Бронювання</li>
+                            <li>Очікування</li>
+                            <li>Понад пробігу</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="thrTarrif">
+                    <div className="redHeader">
+                        <span>
+                            <p>Похвилинная</p>
+                            <h1>23123213123123</h1>
+                        </span>
+                        <p>Найкраще підходить для коротких поїздок</p>
+                    </div>
+                    <div className="addonsCard">
+                        <ul>
+                            <li>Включений пробіг</li>
+                            <li>Бронювання</li>
+                            <li>Очікування</li>
+                            <li>Понад пробігу</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <FooterHome></FooterHome>
         </div>
         </>
     )
